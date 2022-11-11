f244c4d39fb021358107fb780cd6f07279322699
Nyxeia <melothibeault@gmail.com>
2022-11-11 17:41:09 -0500

    ltag

--- Javascript/Champs de profil liens.js
+++ Javascript/Champs de profil liens.js
@@ -10,8 +10,8 @@ $(function() {
 $(function() {
   var el = $('.profile_contact');
     
-    el.find('img[src="https://2img.net/img.icons8.com/small/32/ffffff/name.png"]').replaceWith('<tag>Présentation</tag>');
-    el.find('img[src="https://images2.imgbox.com/ba/03/D1iNQo0J_o.png"]').replaceWith('<tag>Carnet de bord</tag>');
-    el.find('img[src="https://img.icons8.com/ios-glyphs/30/000000/list--v1.png"]').replaceWith('<tag>Page ARGO</tag>');
-    el.find('img[src="https://2img.net/img.icons8.com/small/32/ffffff/communication.png"]').replaceWith('<tag>Message privé</tag>');
+    el.find('img[src="https://2img.net/img.icons8.com/small/32/ffffff/name.png"]').replaceWith('<ltag>Présentation</ltag>');
+    el.find('img[src="https://images2.imgbox.com/ba/03/D1iNQo0J_o.png"]').replaceWith('<ltag>Carnet de bord</ltag>');
+    el.find('img[src="https://img.icons8.com/ios-glyphs/30/000000/list--v1.png"]').replaceWith('<ltag>Page ARGO</ltag>');
+    el.find('img[src="https://2img.net/img.icons8.com/small/32/ffffff/communication.png"]').replaceWith('<ltag>Message privé</ltag>');
   });
