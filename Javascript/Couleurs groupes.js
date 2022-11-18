a7175b0f091d9d9977e9096d5758c61ee0ab0a29
Nyxeia <melothibeault@gmail.com>
2022-11-18 18:44:38 -0500

    Light theme and group colors !

--- Javascript/Couleurs groupes.js
+++ Javascript/Couleurs groupes.js
@@ -1,3 +1,27 @@
-$('a[href^="/u"] span[style="color:#855303"]').each(function(){
-    $(this).addClass("adminColor");
+$(function(){
+
+    $('span[style="color:#7ba9cc"]').each(function(){
+        $(this).addClass("icarus");
+    });
+
+    $('span[style="color:#7baa9a"]').each(function(){
+        $(this).addClass("theseus");
+    });
+
+    $('span[style="color:#C96464"]').each(function(){
+        $(this).addClass("twilight");
+    });
+
+    $('span[style="color:#9b76b8"]').each(function(){
+        $(this).addClass("disaster");
+    });
+
+    $('span[style="color:#d09b72"]').each(function(){
+        $(this).addClass("hybrid");
+    });
+
+    $('span[style="color:#a1a1a1"]').each(function(){
+        $(this).addClass("pnj");
+    });
+
 });
