6c5fcce704d5b92e94f50dbf8a44ebabd0418a3f
Nyxeia <melothibeault@gmail.com>
2024-06-07 22:21:35 -0400

    Départ

--- Javascript/Admins PA.js
+++ Javascript/Admins PA.js
@@ -1,8 +1,8 @@
 var focusColor = getComputedStyle(document.documentElement).getPropertyValue('--gradient');
 var none = "transparent";
 
-var focusIds = ["admin1", "admin2", "admin3", "admin4", "admin5"];
-let adminCount = 5;
+var focusIds = ["admin1", "admin2", "admin3"];
+let adminCount = 3;
 
 
 function changeFocus(position) {
@@ -16,40 +16,24 @@ function changeFocus(position) {
     }
 }
 
-    
 function admin1(){
     changeFocus(1);
-
-    document.getElementById('admin_name').innerHTML="<span>DARIUS | FONDATRICE </span>";
-    document.getElementById('admin_extra').innerHTML="Nyxeia ✧ Présente ✧ Québec";
-
-}
-
-function admin2(){
-    changeFocus(2);
-  
-    document.getElementById('admin_name').innerHTML="<span>LETA | FONDATRICE</span>";
-    document.getElementById('admin_extra').innerHTML="Ellaenys ✧ Présente ✧ France";
-}
-
-function admin3(){
-    changeFocus(3);
   
     document.getElementById('admin_name').innerHTML="<span>MADDOX | ADMINISTRATEUR-MJ</span>";
     document.getElementById('admin_extra').innerHTML="Bones ✧ Présent ✧ France";
 
 }
 
-function admin4(){
-    changeFocus(4);
+function admin2(){
+    changeFocus(2);
   
   document.getElementById('admin_name').innerHTML="<span>ROBIN | ADMINISTRATRICE-MJ</span>";
     document.getElementById('admin_extra').innerHTML="Fern ✧ Présente ✧ Québec";
 
 }
 
-function admin5(){
-    changeFocus(5);
+function admin3(){
+    changeFocus(3);
   
   document.getElementById('admin_name').innerHTML="<span>BELLADONNA | ADMINISTRATRICE-MJ</span>";
     document.getElementById('admin_extra').innerHTML="Valkyrja ✧ Présente ✧ France";
